3af6ce957a9a1f5c9c5b2d9bc930c75c7abb5656
zeeshan-nazar <mzeeshan.nazar@gmail.com>
2016-08-03 13:18:08 +0500

    start working on part1 of this assignment, in this code I pick random word of given word.

--- Hangman.java
+++ Hangman.java
@@ -13,8 +13,38 @@ import java.awt.*;
 
 public class Hangman extends ConsoleProgram {
 
-    public void run() {
+	private int windowWidth = 400;
+	private int windowHeight = 1000;
+	private HangmanLexicon hangman = new HangmanLexicon();
+	
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	
+	private int guessCounter = 8;
+	
+	
+	private void pickWord(){
+		
+		int randWord = rgen.nextInt(0,hangman.getWordCount());
+		
+		String pickWord = hangman.getWord(randWord);
+		
+		println("The Pick word is that :"+ pickWord);
+	}
+	
+	
+	
+	
+	public void run() {
 		/* You fill this in */
+    	setSize(windowWidth,windowHeight);
+    	println("Welcome to Hangman !");
+    	println("The word now looks like this: ");
+    	println("You have " + guessCounter + " guesses left.");
+    	pickWord();
+    	
+    	
+    	
+    	
 	}
 
 }
